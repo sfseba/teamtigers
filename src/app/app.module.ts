635a83c25d08b432753f328c688589804c29ce32
Hyesung Ko <kohs0429@gmail.com>
2021-11-15 22:31:21 -0800

    tiger first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import { ProgramsComponent } from './programs/programs.component';
+import { EventsComponent } from './events/events.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactComponent } from './contact/contact.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FreeComponent } from './free/free.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    AboutComponent,
+    ProgramsComponent,
+    EventsComponent,
+    HeaderComponent,
+    ContactComponent,
+    TopbarComponent,
+    FooterComponent,
+    FreeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
